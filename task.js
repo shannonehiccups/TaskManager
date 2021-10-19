abf694e52212d42749c2207d33885d0ce7b4bb0c
Shannon Hicks <shannonehiccups@gmail.com>
2021-10-19 10:01:26 -0500

    cresated the user input variable

---
+++ task.js
@@ -0,0 +1,14 @@
+// TASK MANAGER 
+
+const menu=`TASK MANAGER 
+
+What would you like to do (Please enter one of the options below):
+"TASKS"-Display all tasks
+"NEW"-Add A New Task
+"REMOVE"- Remove a Task
+"CLOSE"- Close the task manager
+`; 
+
+
+// Displays the menu for the user to select an option. Sets the user's response to the userInput variable
+let userInput =prompt(menu);
